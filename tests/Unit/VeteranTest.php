bbfe6381f984aaf75befdbf77a19cdd7f922f25a
Stepan <steptarget@yandex.ru>
2023-07-11 02:27:20 +0700

    Relocate and correct Veteran, correct VeteranTest (namespaces)

--- tests/Unit/VeteranTest.php
+++ tests/Unit/VeteranTest.php
@@ -1,20 +1,17 @@
 <?php
-require_once '/Users/admin/PhpstormProjects/VeteranGibdd/core/class/Veteran.php';
 
+namespace Gibdd\Core\Tests\Unit;
+
+use Gibdd\Core\Veteran;
 use PHPUnit\Framework\TestCase;
 
 class VeteranTest extends TestCase
 {
-    private Veteran $veteran;
-
-    protected function setUp(): void
-    {
-        $this->veteran = new Veteran(['first_name' => 'Ivan', 'last_name' => 'Ivanov']);
-    }
-
     public function testJsonSerialize()
     {
-        $this->assertSame(json_encode(['first_name' => 'Ivan', 'last_name' => 'Ivanov']), json_encode($this->veteran));
+        $veteran = new Veteran(['first_name' => 'Ivan', 'last_name' => 'Ivanov']);
+
+        $this->assertSame(json_encode(['first_name' => 'Ivan', 'last_name' => 'Ivanov']), json_encode($veteran));
     }
 
 }
