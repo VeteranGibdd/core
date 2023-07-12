8d1c24a9b5642e30da8c2c4829553060be047968
Stepan <steptarget@yandex.ru>
2023-07-13 01:48:42 +0700

    Rolled back commit and push one wiwhout DB

---
+++ phinx.php
@@ -0,0 +1,28 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'production' => [
+            'adapter' => 'sqlite',
+            'name' => './db/veterans',
+            'suffix' => '.sqlite3',
+        ],
+        'development' => [
+            'adapter' => 'sqlite',
+            'name' => './db/veterans',
+            'suffix' => '.sqlite3',
+        ],
+        'testing' => [
+            'adapter' => 'sqlite',
+            'memory' => 'true',
+        ]
+    ],
+    'version_order' => 'creation'
+];
