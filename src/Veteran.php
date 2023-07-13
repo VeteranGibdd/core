8e5065dc57bf6ed1ed00e4a2fc2909b751729989
Stepan <steptarget@yandex.ru>
2023-07-14 04:02:13 +0700

    Adjust Veteran class to new structure and correct VeteranTest

--- src/Veteran.php
+++ src/Veteran.php
@@ -2,6 +2,8 @@
 
 namespace Gibdd\Core;
 
+use IntlDateFormatter;
+use DateTime;
 use JsonSerializable;
 
 class Veteran implements JsonSerializable
@@ -15,7 +17,78 @@ class Veteran implements JsonSerializable
 
     public function jsonSerialize(): array
     {
-        return $this->dbRow;
+        /**
+         * Count age from birthday
+         * @param string $birthday
+         * @return int
+         */
+        function calculateAge(string $birthday): int
+        {
+            $birthday_timestamp = strtotime($birthday);
+            $age = date('Y') - date('Y', $birthday_timestamp);
+            if (date('md', $birthday_timestamp) > date('md')) {
+                $age--;
+            }
+            return $age;
+        }
+
+        /**
+         * Add "год, года, лет" to year
+         * @param int $year
+         * @return string
+         */
+        function yearTextArg(int $year): string
+        {
+            $year = abs($year);
+            $t1 = $year % 10;
+            $t2 = $year % 100;
+            return $year . ' ' . ($t1 == 1 && $t2 != 11 ? "год" : ($t1 >= 2 && $t1 <= 4 && ($t2 < 10 || $t2 >= 20) ? "года" : "лет"));
+        }
+
+        /**
+         * Reformat date
+         * @param DateTime $date
+         * @return string
+         */
+        function getDate(DateTime $date): string
+        {
+            $intlFormatter = new IntlDateFormatter('ru_RU', IntlDateFormatter::MEDIUM, IntlDateFormatter::MEDIUM);
+            $intlFormatter->setPattern('dd MMMM yyyy' . ' г.');
+            return $intlFormatter->format($date);
+        }
+
+        $birthdate = new DateTime($this->dbRow['birth_date']);
+        $validity = new DateTime($this->dbRow['certificate_validity']);
+
+        return [
+            'id' => $this->dbRow['id'],
+            'firstname' => $this->dbRow['first_name'],
+            'lastname' => $this->dbRow['last_name'],
+            'middlename' => $this->dbRow['middle_name'],
+            'birthdate' => getDate($birthdate),
+            'age' => yearTextArg(calculateAge($this->dbRow['birth_date'])),
+            'liveaddress' => $this->dbRow['live_address'],
+            'paspaddress' => $this->dbRow['passport_address'],
+            'rank' => $this->dbRow['rank'],
+            'lengthservice' => yearTextArg($this->dbRow['length_service']),
+            'lengthservicepolice' => yearTextArg($this->dbRow['length_service_police']),
+            'retirementstatus' => $this->dbRow['retirement_status'],
+            'retirementyear' => $this->dbRow['retirement_year'],
+            'certnumber' => $this->dbRow['certificate_number'],
+            'validity' => getDate($validity),
+            'status' => $this->dbRow['status'],
+            'yearentrytopolice' => $this->dbRow['year_entry_to_police'],
+            'duty' => $this->dbRow['duty'],
+            'mobphone' => $this->dbRow['mobile_phone'],
+            'reservephone' => $this->dbRow['reserve_phone'],
+            'email' => $this->dbRow['email'],
+            'passport' => $this->dbRow['passport'],
+            'awards' => $this->dbRow['awards'],
+            'disability' => $this->dbRow['disability'],
+            'participation' => $this->dbRow['hostilities_participation'],
+            'additionally' => $this->dbRow['additionally'],
+            'yeardismissal' => $this->dbRow['year_of_dismissal'],
+        ];
     }
 
 }
--- tests/Unit/VeteranTest.php
+++ tests/Unit/VeteranTest.php
@@ -9,9 +9,68 @@ class VeteranTest extends TestCase
 {
     public function testJsonSerialize()
     {
-        $veteran = new Veteran(['first_name' => 'Ivan', 'last_name' => 'Ivanov']);
+        $vetData = [
+            'id' => 1,
+            'first_name' => 'Джон',
+            'last_name' => 'Тестовый',
+            'middle_name' => 'Деппович',
+            'birth_date' => '1978-05-27',
+            'live_address' => 'Хоста',
+            'passport_address' => 'г.Сочи, Хоста, п. Кудепста, ул. Калиновая, д.6',
+            'rank' => 'Капитан полиции',
+            'length_service' => 31,
+            'length_service_police' => 20,
+            'retirement_status' => 'В отставке',
+            'retirement_year' => 2018,
+            'certificate_number' => 'ККС № 0205',
+            'certificate_validity' => '2023-07-03',
+            'status' => 'Ветеран',
+            'year_entry_to_police' => 2012,
+            'duty' => 'Актив',
+            'mobile_phone' => 89881234567,
+            'reserve_phone' => 2123456,
+            'email' => 'test@mail.ru',
+            'passport' => '03 20 682534',
+            'awards' => 'за выслугу лет',
+            'disability' => '2 группа',
+            'hostilities_participation' => 'Карабах',
+            'additionally' => 'Большой молодец',
+            'year_of_dismissal' => 2022,
+        ];
 
-        $this->assertSame(json_encode(['first_name' => 'Ivan', 'last_name' => 'Ivanov']), json_encode($veteran));
+        $vetDataJson = [
+            'id' => 1,
+            'firstname' => 'Джон',
+            'lastname' => 'Тестовый',
+            'middlename' => 'Деппович',
+            'birthdate' => '27 мая 1978 г.',
+            'age' => '45 лет',
+            'liveaddress' => 'Хоста',
+            'paspaddress' => 'г.Сочи, Хоста, п. Кудепста, ул. Калиновая, д.6',
+            'rank' => 'Капитан полиции',
+            'lengthservice' => '31 год',
+            'lengthservicepolice' => '20 лет',
+            'retirementstatus' => 'В отставке',
+            'retirementyear' => 2018,
+            'certnumber' => 'ККС № 0205',
+            'validity' => '03 июля 2023 г.',
+            'status' => 'Ветеран',
+            'yearentrytopolice' => 2012,
+            'duty' => 'Актив',
+            'mobphone' => 89881234567,
+            'reservephone' => 2123456,
+            'email' => 'test@mail.ru',
+            'passport' => '03 20 682534',
+            'awards' => 'за выслугу лет',
+            'disability' => '2 группа',
+            'participation' => 'Карабах',
+            'additionally' => 'Большой молодец',
+            'yeardismissal' => 2022,
+        ];
+
+        $veteran = new Veteran($vetData);
+
+        $this->assertSame(json_encode($vetDataJson, JSON_UNESCAPED_UNICODE), json_encode($veteran, JSON_UNESCAPED_UNICODE));
     }
 
 }
