0c763015107c795f9dbfe4e1eab60ce3cbd2cab6
Stepan <steptarget@yandex.ru>
2023-07-14 04:19:42 +0700

    Adjust Veteran class to new structure and correct VeteranTest (use anonymus function)

--- src/Veteran.php
+++ src/Veteran.php
@@ -22,7 +22,7 @@ class Veteran implements JsonSerializable
          * @param string $birthday
          * @return int
          */
-        function calculateAge(string $birthday): int
+        $calculateAge = function (string $birthday): int
         {
             $birthday_timestamp = strtotime($birthday);
             $age = date('Y') - date('Y', $birthday_timestamp);
@@ -30,32 +30,32 @@ class Veteran implements JsonSerializable
                 $age--;
             }
             return $age;
-        }
+        };
 
         /**
          * Add "год, года, лет" to year
          * @param int $year
          * @return string
          */
-        function yearTextArg(int $year): string
+        $yearTextArg = function (int $year): string
         {
             $year = abs($year);
             $t1 = $year % 10;
             $t2 = $year % 100;
             return $year . ' ' . ($t1 == 1 && $t2 != 11 ? "год" : ($t1 >= 2 && $t1 <= 4 && ($t2 < 10 || $t2 >= 20) ? "года" : "лет"));
-        }
+        };
 
         /**
          * Reformat date
          * @param DateTime $date
          * @return string
          */
-        function getDate(DateTime $date): string
+        $date = function (DateTime $date): string
         {
             $intlFormatter = new IntlDateFormatter('ru_RU', IntlDateFormatter::MEDIUM, IntlDateFormatter::MEDIUM);
             $intlFormatter->setPattern('dd MMMM yyyy' . ' г.');
             return $intlFormatter->format($date);
-        }
+        };
 
         $birthdate = new DateTime($this->dbRow['birth_date']);
         $validity = new DateTime($this->dbRow['certificate_validity']);
@@ -65,17 +65,17 @@ class Veteran implements JsonSerializable
             'firstname' => $this->dbRow['first_name'],
             'lastname' => $this->dbRow['last_name'],
             'middlename' => $this->dbRow['middle_name'],
-            'birthdate' => getDate($birthdate),
-            'age' => yearTextArg(calculateAge($this->dbRow['birth_date'])),
+            'birthdate' => $date($birthdate),
+            'age' => $yearTextArg($calculateAge($this->dbRow['birth_date'])),
             'liveaddress' => $this->dbRow['live_address'],
             'paspaddress' => $this->dbRow['passport_address'],
             'rank' => $this->dbRow['rank'],
-            'lengthservice' => yearTextArg($this->dbRow['length_service']),
-            'lengthservicepolice' => yearTextArg($this->dbRow['length_service_police']),
+            'lengthservice' => $yearTextArg($this->dbRow['length_service']),
+            'lengthservicepolice' => $yearTextArg($this->dbRow['length_service_police']),
             'retirementstatus' => $this->dbRow['retirement_status'],
             'retirementyear' => $this->dbRow['retirement_year'],
             'certnumber' => $this->dbRow['certificate_number'],
-            'validity' => getDate($validity),
+            'validity' => $date($validity),
             'status' => $this->dbRow['status'],
             'yearentrytopolice' => $this->dbRow['year_entry_to_police'],
             'duty' => $this->dbRow['duty'],
